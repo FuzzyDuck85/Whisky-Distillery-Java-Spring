9f1076ca43d279566ea9f2625e43e55dc20183a1
Donald Cameron <drc85@live.co.uk>
2020-07-29 14:34:59 +0100

    started on MVP, filter by region and year working

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,38 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class WhiskyController {
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> findWhiskiesFilterByYear(
+            @RequestParam(name = "year", required = false) int year
+    ){
+        if (year != 0){
+            return new ResponseEntity<>(whiskyRepository.findWhiskiesByYear(year), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/whiskies/{id}")
+    public ResponseEntity getWhiskys(@PathVariable Long id){
+        return new ResponseEntity<>(whiskyRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/whiskies")
+    public ResponseEntity<Whisky> postWhisky(@RequestBody Whisky whisky){
+        whiskyRepository.save(whisky);
+        return new ResponseEntity<>(whisky, HttpStatus.CREATED);
+    }
+
+}
