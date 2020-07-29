9d074fbf26f807064369b3515d2f290728e7cc0a
Donald Cameron <drc85@live.co.uk>
2020-07-29 15:32:27 +0100

    MVP completed

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -3,6 +3,7 @@ package com.codeclan.example.WhiskyTracker.controllers;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +17,22 @@ public class WhiskyController {
 
     @GetMapping(value = "/whiskies")
     public ResponseEntity<List<Whisky>> findWhiskiesFilterByYear(
-            @RequestParam(name = "year", required = false) int year
+            @RequestParam(name = "year", required = false) Integer year,
+            @RequestParam(name="distillery", required = false) String distillery,
+            @RequestParam(name = "age", required = false) Integer age,
+            @RequestParam(name = "name", required = false) String name
     ){
-        if (year != 0){
+        if (year != null){
             return new ResponseEntity<>(whiskyRepository.findWhiskiesByYear(year), HttpStatus.OK);
+        }
+        if (age != null){
+            return new ResponseEntity<>(whiskyRepository.findWhiskiesByAge(age), HttpStatus.OK);
+        }
+        if (age != null && age != null){
+            return new ResponseEntity<>(whiskyRepository.findByDistilleryAndAge(distillery, age), HttpStatus.OK);
+        }
+        if (name != null){
+            return new ResponseEntity<>(whiskyRepository.findWhiskiesByName(name), HttpStatus.OK);
         }
         return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
     }
@@ -34,5 +47,6 @@ public class WhiskyController {
         whiskyRepository.save(whisky);
         return new ResponseEntity<>(whisky, HttpStatus.CREATED);
     }
+
 
 }
--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -1,6 +1,8 @@
 package com.codeclan.example.WhiskyTracker.repositories;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.sun.codemodel.internal.JWhileLoop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,9 @@ import java.util.List;
 
 @Repository
 public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
-    List<Whisky> findWhiskiesByYear(int age);
+    List<Whisky> findWhiskiesByYear(Integer year);
+    List<Whisky> findWhiskiesByAge(Integer age);
+    List<Whisky> findWhiskiesByName(String name);
+    List<Whisky> findByDistilleryAndAge(String name, int age);
 
 }
